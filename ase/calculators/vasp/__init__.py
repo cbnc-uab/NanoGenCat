4bbfb4c4d74343f2ccb8f97702c3f9a3d4117c67
Sergi Pérez Labernia <rndrnd@gmail.com>
2018-09-14 17:35:01 +0200

    DEL ase folder

--- ase/calculators/vasp/__init__.py
+++
@@ -1,4 +0,0 @@
-from .vasp import Vasp, VaspChargeDensity, VaspDos, xdat2traj
-from .interactive import VaspInteractive
-__all__ = ['Vasp', 'VaspChargeDensity', 'VaspDos', 'xdat2traj',
-           'VaspInteractive']
