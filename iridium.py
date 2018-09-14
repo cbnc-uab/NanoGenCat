614dcce2c9cea3c06301f9355d37912647e13ded
Sergi Pérez Labernia <rndrnd@gmail.com>
2018-09-14 17:44:20 +0200

    MOD import names

--- iridium.py
+++ iridium.py
@@ -1,8 +1,15 @@
+import sys
+import os
+
+####
+sys.path.append(os.path.abspath("bcnm/"))
+
 from ase.spacegroup import crystal
 from ase.visualize import view
-from ase.cluster import wulff_construction
+from bcn_wulff import bcn_wulff_construction
 from ase.build import cut
 from ase.io import  write, read
+
 import numpy as np
 
 basis=[(0.0,0.0,0.0),(0.306, 0.306, 0.0)]
@@ -16,7 +23,7 @@ surfaces = [(1,1,0),(0,1,1),(1,0,0),(0,0,1)]
 esurf=[0.94,1.06,1.23,1.55]
 # for size in np.arange(20,30,1):
 size= 13.
-atoms = wulff_construction(iridium,surfaces,esurf,float(size),'ext',rounding='above',debug=1)
+atoms = bcn_wulff_construction(iridium,surfaces,esurf,float(size),'ext',rounding='above',debug=1)
 
 exit()
 #
