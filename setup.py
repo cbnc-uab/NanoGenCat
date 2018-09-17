4630d098a47a15dcedb6cf600bda5ddc2e8bcf26
Sergi Pérez Labernia <rndrnd@gmail.com>
2018-09-17 11:13:40 +0200

    Fix setup information errors

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
  long_description = fh.read()
 
-setuptools.setup(
- name="bcnm",
- version="0.1.0",
- author="bruno.camino@uab.cat",
- author="dagonzalez@qf.uab.cat",
- author_email="bcnm@qf.uab.cat",
- description="BcnM is a computational tool to obtain realistic models of nanoparticles from bulk systems of any type of material",
+setup(
+ name='bcnm',
+ version='0.1.0',
+ author='Bruno Camino, Danilo González Forero',
+ author_email='bcnm@qf.uab.cat',
+ description='BcnM is a computational tool to obtain realistic models of nanoparticles from bulk systems of any type of material',
  long_description=long_description,
- url="https://github.com/dagonzalezfo/NanoGenCat",
- packages=setuptools.find_packages(),
+ url='https://github.com/dagonzalezfo/NanoGenCat',
+ packages=find_packages(),
  classifiers=[
-  "Programming Language :: Python :: 3"
-  "Licences :: GNU General Public License GPLv3",
-  "Operating System :: OS Independent",
+  'Programming Language :: Python',
+  'Development Status :: Pre-Alpha',
+  'Natural Language :: English',
+  'Intended Audience :: Science/Research',
+  'Licences :: GNU General Public License GPLv3',
+  'Operating System :: OS Independent',
+  'Topic :: Scientific/Engineering :: Chemistry',
  ],
+ install_requires=['ase'],
 ) 
