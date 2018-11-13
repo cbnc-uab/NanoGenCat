bfa5637252da8ab3c30be4848d77c131a4469696
dagonzalezfo <dagonzalezfo@gmail.com>
2018-11-13 16:50:28 +0100

    diridium to build magnetic nanoparticles

--- dtitanium.py
+++ dtitanium.py
@@ -6,20 +6,19 @@ from ase.build import cut
 from ase.io import  write, read
 import numpy as np
 
-# basis=[(0.0,0.0,0.0),(0.306, 0.306, 0.0)]
-# a= 4.5424  
-# c= 3.1862   
-# iridium = crystal('IrO', basis, spacegroup=136, cellpar=[a,a,c,90,90,90],primitive_cell=False)
-# print(iridium)
-# write('crystalShape',iridium,format='xyz')
+basis=[(0.0,0.0,0.0),(0.306, 0.306, 0.0)]
+a= 4.603  
+c= 2.977   
+iridium = crystal('TiO', basis, spacegroup=136, cellpar=[a,a,c,90,90,90],primitive_cell=False)
+print(iridium)
+write('crystalShape',iridium,format='xyz')
 # view(iridium)
-iridium=read('crystalShape')
 
 surfaces = [(1,1,0),(0,1,1),(1,0,0),(0,0,1)]
-esurf=[0.94,1.06,1.23,1.55]
+esurf=[0.014,0.044,0.022,0.057]
 # for size in np.arange(20,30,1):
-size= 14
-atoms = bcn_wulff_construction(iridium,surfaces,esurf,float(size),'ext',rounding='above',debug=1)
+size= 18
+atoms = bcn_wulff_construction(iridium,surfaces,esurf,float(size),'ext',rounding='above',debug=1,option=1)
 # atoms = wulff_construction(iridium,surfaces,esurf,float(size),'ext',rounding='above',debug=1)
 
 exit()
